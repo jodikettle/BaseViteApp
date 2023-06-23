abaccf9a0276e90b7d9ccfd9831d616e1735477f
REDINGTON\Harry.Billington <harry.billington@redington.co.uk>
2023-06-23 10:38:11 +0100

    game start

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Typography } from '@mui/material'
 import { CenteredContent } from '@/components/CenteredContent/CenteredContent'
 import { Colors } from '@/constants/styles'
@@ -7,16 +7,26 @@ import { PlayCard } from '@/components/PlayCard/PlayCard'
 
 export const HomePage: FC = () => {
   const items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  return (
-    <MainPageWrapper>
-      <CenteredContent>
-        <Typography variant="h4" fontWeight={800} color={[Colors.RichBlack]}>
-          Memory Game
-        </Typography>
-        {items.map((item) => (
-          <PlayCard key={item} />
-        ))}
-      </CenteredContent>
-    </MainPageWrapper>
-  )
+  const [gameStarted, setGameStart] = useState(false)
+
+  const startTheGame = () => {
+    setGameStart(true)
+  }
+
+  if (gameStarted) {
+    return (
+      <MainPageWrapper>
+        <CenteredContent>
+          <Typography variant="h4" fontWeight={800} color={[Colors.RichBlack]}>
+            Memory Game
+          </Typography>
+          {items.map((item) => (
+            <PlayCard key={item} />
+          ))}
+        </CenteredContent>
+      </MainPageWrapper>
+    )
+  } else {
+    return <RocketIcon onClick={startTheGame} />
+  }
 }
