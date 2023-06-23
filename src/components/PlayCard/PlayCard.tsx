1cc61afa35c9e9110da647d6b12af6935dbff034
REDINGTON\Harry.Billington <harry.billington@redington.co.uk>
2023-06-23 11:06:08 +0100

    basic 3x3 styling

--- src/components/PlayCard/PlayCard.tsx
+++ src/components/PlayCard/PlayCard.tsx
@@ -8,11 +8,8 @@ const onSquareClick = () => {
 interface Props { }
 
 const PlayCardButton = styled.button`
-  grid-column-start: 1;
   color: white;
   background-color: DodgerBlue;
-  width: 40px;
-  height: 40px;
 `
 
 export const PlayCard: React.FC<Props> = (props) => {
--- src/components/PlayCard/PlayCardContainer.tsx
+++ src/components/PlayCard/PlayCardContainer.tsx
@@ -2,4 +2,7 @@ import styled from '@emotion/styled'
 
 export const PlayCardContainer = styled.div`
   display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
 `
